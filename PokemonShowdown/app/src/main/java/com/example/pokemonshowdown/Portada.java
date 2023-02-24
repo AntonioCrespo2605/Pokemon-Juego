696f2c128fb9cb15c0a2c2ffb5cd1d11865769a7
unknown <antoniocrespgome@gmail.com>
2023-02-24 21:47:42 +0100

    adding an animation to the splash screen´s pokeball

--- PokemonShowdown/app/src/main/java/com/example/pokemonshowdown/Portada.java
+++ PokemonShowdown/app/src/main/java/com/example/pokemonshowdown/Portada.java
@@ -5,20 +5,29 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.RotateAnimation;
 import android.widget.ImageView;
-
 import java.util.Timer;
 import java.util.TimerTask;
 
 public class Portada extends AppCompatActivity {
 
-    private ImageView portada;
+    private ImageView portada, pokeball;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_portada);
         portada = (ImageView) findViewById(R.id.portada);
+        pokeball=(ImageView)findViewById(R.id.pokeball);
+
+        RotateAnimation rotateAnimation=new RotateAnimation(0, 1440, RotateAnimation.RELATIVE_TO_SELF,
+                .5f, RotateAnimation.RELATIVE_TO_SELF
+                ,.5f);
+
+
+        rotateAnimation.setDuration(5000);
+        pokeball.startAnimation(rotateAnimation);
 
         super.onStart();
         final Timer t = new Timer();
