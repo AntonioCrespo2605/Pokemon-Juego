c4822d1918cabec97acc2c6f1e2c76600aa5d78f
aitana-vidal <91663760+aitana-vidal@users.noreply.github.com>
2023-02-25 15:40:59 +0100

    Activity Combat
    combat xml

---
+++ PokemonShowdown/app/src/main/java/com/example/pokemonshowdown/Combat.java
@@ -0,0 +1,14 @@
+package com.example.pokemonshowdown;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Combat extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_combat);
+    }
+}
