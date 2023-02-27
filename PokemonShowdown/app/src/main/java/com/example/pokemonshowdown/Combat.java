742cf34efa3835aaafef12f427cf6586d621ef50
unknown <antoniocrespgome@gmail.com>
2023-02-28 00:04:13 +0100

    fixing images

--- PokemonShowdown/app/src/main/java/com/example/pokemonshowdown/Combat.java
+++ PokemonShowdown/app/src/main/java/com/example/pokemonshowdown/Combat.java
@@ -20,6 +20,7 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton;
 
